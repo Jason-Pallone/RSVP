813e63665bfae5a818aff54eeaa650d7318eca80
Jason-Pallone <jasonpallone7593@gmail.com>
2019-06-01 14:17:50 -0400

    more refactoring of MainContent.js

--- RSVP/src/components/MainContent.js
+++ RSVP/src/components/MainContent.js
@@ -5,15 +5,44 @@ import GuestList from './GuestList';
 import PendingGuestLi from './PendingGuestLi';
 
 const MainContent=(props)=>{
-
-    const guests = props.guest
     {/* 
     checks to see if the first li has been created, if not created shows the intro message and doesn't 
     dispaly the filter checkbox, else if first li is created, hides intro message and shows filter checkbox
      */}
+
+     const guests = props.guest
+
      if (guests.length < 1) {
+
         return( 
-      <div className="App">
+
+          <div className="App">
+  
+              <Header />
+              <AddGuest
+              guest={props.guest}
+              addGuest={props.addGuest}
+              updatePendingGuestValue={props.updatePendingGuestValue}
+              pendingGuest={props.pendingGuest}
+              />
+
+              <p className='intro-message'>
+              Invite some friends and family! Confirm your guests,
+              change their names if needed, remove the guest or filter through your guest who
+              have/have not confirmed. Get started above!
+              </p>
+
+              <PendingGuestLi
+                pendingGuest={props.pendingGuest}
+              />
+          </div>
+            )
+   } else {
+
+      return(
+
+    <div className="App">
+    
         <Header />
         <AddGuest
         guest={props.guest}
@@ -21,46 +50,27 @@ const MainContent=(props)=>{
         updatePendingGuestValue={props.updatePendingGuestValue}
         pendingGuest={props.pendingGuest}
         />
-       <p className='intro-message'>
-       Invite some friends and family! Confirm your guests,
-       change their names if needed, remove the guest or filter through your guest who
-       have/have not confirmed. Get started above!
-       </p>
 
-       <PendingGuestLi
-         pendingGuest={props.pendingGuest} />
-      </div>
-        )
-   } else {
-      return(
-    <div className="App">
-      <Header />
-      <AddGuest
-      guest={props.guest}
-      addGuest={props.addGuest}
-      updatePendingGuestValue={props.updatePendingGuestValue}
-      pendingGuest={props.pendingGuest}
-      />
+        <div className='filter-checkbox-container'>
+          <label className='filter-guests-label'>
+              Hide all unconfirmed guest
+            <input type='checkbox' className='filter-guests-checkbox'
+            value={props.filterUnconfirmedGuest} onChange={props.toggleFilterUnconfirmedGuest} />
+          </label>
+        </div>
+    
+        <GuestList
+          guest={props.guest}
+          toggleGuestConfirmed={props.toggleGuestConfirmed}
+          toggleEditGuest={props.toggleEditGuest}
+          editGuest={props.editGuest}
+          removeGuest={props.removeGuest}
+          filterUnconfirmedGuest={props.filterUnconfirmedGuest}
+            />
 
-      <div className='filter-checkbox-container'>
-        <label className='filter-guests-label'>
-            Hide all unconfirmed guest
-          <input type='checkbox' className='filter-guests-checkbox'
-          value={props.filterUnconfirmedGuest} onChange={props.toggleFilterUnconfirmedGuest} />
-        </label>
-      </div>
-  
-      <GuestList
-        guest={props.guest}
-        toggleGuestConfirmed={props.toggleGuestConfirmed}
-        toggleEditGuest={props.toggleEditGuest}
-        editGuest={props.editGuest}
-        removeGuest={props.removeGuest}
-        filterUnconfirmedGuest={props.filterUnconfirmedGuest}
-          />
-
-      <PendingGuestLi
-         pendingGuest={props.pendingGuest} />
+        <PendingGuestLi
+          pendingGuest={props.pendingGuest} 
+        />
     </div>
       )
     }
